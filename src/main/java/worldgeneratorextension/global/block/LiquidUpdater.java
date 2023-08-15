f8e2549187c338dd030bafa0a41fd674ebe7f516
Petteri <26197131+PetteriM1@users.noreply.github.com>
2023-08-15 20:30:08 +0300

    Schedule update for lava

--- src/main/java/worldgeneratorextension/global/block/LiquidUpdater.java
+++ src/main/java/worldgeneratorextension/global/block/LiquidUpdater.java
@@ -1,8 +1,8 @@
 package worldgeneratorextension.global.block;
-
-import cn.nukkit.Server;
 import cn.nukkit.block.Block;
 import cn.nukkit.level.ChunkManager;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.math.BlockVector3;
 
 public final class LiquidUpdater {
@@ -12,8 +12,12 @@ public final class LiquidUpdater {
     }
 
     private static void lavaSpread(ChunkManager level, int x, int y, int z, int counter) {
-        if (counter > 200) {
-            Server.getInstance().getLogger().debug("LiquidUpdater#lavaSpread: too many iterations");
+        if (counter > 100) {
+            FullChunk chunk;
+            if (level instanceof Level && (chunk = level.getChunk(x >> 4, z >> 4)) != null) {
+                Block block = Block.get(((Level) level).getFullBlock(chunk, x, y, z), (Level) level, x, y, z);
+                ((Level) level).scheduleUpdate(block, block, 10, 0, false);
+            }
             return;
         }
 
