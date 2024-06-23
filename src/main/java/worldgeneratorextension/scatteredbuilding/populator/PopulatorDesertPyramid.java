8abf746084f035c2acd21b559b09b80ff94c58a9
Petteri <26197131+PetteriM1@users.noreply.github.com>
2024-06-23 13:58:04 +0300

    Update PopulatorDesertPyramid

--- src/main/java/worldgeneratorextension/scatteredbuilding/populator/PopulatorDesertPyramid.java
+++ src/main/java/worldgeneratorextension/scatteredbuilding/populator/PopulatorDesertPyramid.java
@@ -11,7 +11,7 @@ public class PopulatorDesertPyramid extends PopulatorScatteredStructure {
     @Override
     protected boolean canGenerate(int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         int biome = chunk.getBiomeId(7, 7);
-        return (biome == EnumBiome.DESERT.id || biome == EnumBiome.BEACH.id || biome == EnumBiome.DESERT_HILLS.id || biome == EnumBiome.DESERT_M.id) && super.canGenerate(chunkX, chunkZ, random, chunk);
+        return (biome == EnumBiome.DESERT.id || biome == EnumBiome.DESERT_HILLS.id || biome == EnumBiome.DESERT_M.id) && super.canGenerate(chunkX, chunkZ, random, chunk);
     }
 
     @Override
